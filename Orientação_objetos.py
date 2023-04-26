bb6c9a78833e814bdec4e41385fe64e082538dfa
intCadu <kadurochagomes@gmail.com>
2023-04-26 16:38:14 -0300

    Update Orientação_objetos.py

--- Orientação_objetos.py
+++ Orientação_objetos.py
@@ -50,47 +50,91 @@
 # Crie alguns objetos dessas classes e teste o método ligar() em cada um.
 
 
-class Veiculo:
-    def __init__(self, modelo, cor, ano, ligado):
-        self.modelo = modelo
-        self.cor = cor
-        self.ano = ano
-        self.ligado = ligado
+# class Veiculo:
+#     def __init__(self, modelo, cor, ano, ligado):
+#         self.modelo = modelo
+#         self.cor = cor
+#         self.ano = ano
+#         self.ligado = ligado
     
-    def ligar(self):
-        if self.ligado == "ON":
-            print(f"O {self.modelo} está ligado!")
-        else:
-            print(f"O {self.modelo} está desligado!")
+#     def ligar(self):
+#         if self.ligado == "ON":
+#             print(f"O {self.modelo} está ligado!")
+#         else:
+#             print(f"O {self.modelo} está desligado!")
 
-class Carro(Veiculo):
-    def __init__(self, modelo, cor, ano, ligado):
-        super().__init__(modelo, cor, ano, ligado)
-    def ligar(self):
-        if self.ligado == "ON":
-            print(f"O {self.modelo} está ligado!")
-        else:
-            print(f"O {self.modelo} está desligado!")
+# class Carro(Veiculo):
+#     def __init__(self, modelo, cor, ano, ligado):
+#         super().__init__(modelo, cor, ano, ligado)
+#     def ligar(self):
+#         if self.ligado == "ON":
+#             print(f"O {self.modelo} está ligado!")
+#         else:
+#             print(f"O {self.modelo} está desligado!")
 
-class Moto(Veiculo):
-    def __init__(self, modelo, cor, ano, ligado):
-        super().__init__(modelo, cor, ano, ligado)
-    def ligar(self):
-        if self.ligado == "ON":
-            print(f"O {self.modelo} está ligado!")
-        else:
-            print(f"O {self.modelo} está desligado!")
+# class Moto(Veiculo):
+#     def __init__(self, modelo, cor, ano, ligado):
+#         super().__init__(modelo, cor, ano, ligado)
+#     def ligar(self):
+#         if self.ligado == "ON":
+#             print(f"O {self.modelo} está ligado!")
+#         else:
+#             print(f"O {self.modelo} está desligado!")
 
-class Bicicleta(Veiculo):
-    def __init__(self, modelo, cor, ano, ligado):
-        super().__init__(modelo, cor, ano, ligado)
-    def ligar(self):
-        if self.ligado == "ON":
-            print(f"O {self.modelo} está ligado!")
-        else:
-            print(f"O {self.modelo} está desligado!")
+# class Bicicleta(Veiculo):
+#     def __init__(self, modelo, cor, ano, ligado):
+#         super().__init__(modelo, cor, ano, ligado)
+#     def ligar(self):
+#         if self.ligado == "ON":
+#             print(f"O {self.modelo} está ligado!")
+#         else:
+#             print(f"O {self.modelo} está desligado!")
 
-carro1 = Carro("UNO", "Preto", "1995", "ON")
-moto1 = Moto("FAN", "Vermelho", "2013", "OFF")
-bicicleta1 = Bicicleta("CALOI", "Azul", "2018", "ON")
+# carro1 = Carro("UNO", "Preto", "1995", "ON")
+# moto1 = Moto("FAN", "Vermelho", "2013", "OFF")
+# bicicleta1 = Bicicleta("CALOI", "Azul", "2018", "ON")
         
+# carro1.ligar()
+# moto1.ligar()
+# bicicleta1.ligar()
+
+
+class Conta:
+    def __init__(self, saldo, conta):
+        self.saldo = saldo
+        self.conta = conta
+
+    def get_Saldo(self):
+        return self.saldo
+    
+    def get_Conta(self):
+        return self.conta 
+    
+    def set_Saldo(self, novoSaldo):
+        novoSaldo = input("Digite o valor do seu novo saldo:")
+        self.saldo = novoSaldo
+        return novoSaldo
+    
+    def set_Conta(self, novaConta):
+        novaConta = input("Digite o numero da sua nova conta:")
+        self.conta = novaConta
+        return novaConta
+
+    def _deposito(self):
+        x = input("Deseja fazer um deposito na sua conta?(S/N)")
+        if x.upper() == "S":
+            deposito = int(input("Digite o valor do deposito que deseja fazer:"))
+            saldo_deposito = deposito + self.saldo
+            print(saldo_deposito)
+        else:
+            print("Moetodo de deposito ENCERRADO!")
+
+conta1 = Conta(400, 12345)
+
+# print(conta1.get_Saldo())
+# print(conta1.get_Conta())
+
+# print(conta1.set_Conta(98765))
+# print(conta1.set_Saldo(1500))
+
+conta1._deposito()
